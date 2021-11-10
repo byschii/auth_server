e75369d557ef9b8f3a1fc0c3f74b19b5410ca5ef
scalamat <matteo.scala@hln.it>
2021-11-10 11:22:03 +0100

    aggiunto file di ocnfigurazione

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
---
+++ utils/configuration.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/go-yaml/yaml"
+)
+
+type Configuration struct {
+	DbFileName string
+}
+
+func LoadConfiguration(fileName string) Configuration {
+	var config Configuration
+	var confFileContent, err = os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(confFileContent, config)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
